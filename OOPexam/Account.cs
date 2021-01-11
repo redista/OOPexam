01247124034b726c339b7df2fc8826aed0a31023
redista <redista@users.github.com>
2021-01-11 09:48:58 +0000

    added InterestRate to both inherited classes

--- OOPexam/Account.cs
+++ OOPexam/Account.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace OOPexam
 {
-    class Class1
+    public abstract class Account
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime InterestDate { get; set; }
+
+        public abstract decimal CalculateInterest(); 
+    }
+
+    public class CurrentAccount : Account
+    {
+        public decimal InterestRate = 0.03m;
+
+        public override decimal CalculateInterest()
+        {
+            
+        }
+    }
+    
+    public class SavingsAccount : Account
+    {
+        public decimal InterestRate = 0.06m;
     }
 }
