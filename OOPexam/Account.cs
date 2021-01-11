9067b9538192502c3c67a38297962b17caa62393
redista <redista@users.github.com>
2021-01-11 11:16:54 +0000

    Finished. Added comments.

--- OOPexam/Account.cs
+++ OOPexam/Account.cs
@@ -8,6 +8,7 @@ namespace OOPexam
 {
     public abstract class Account
     {
+        // Right now the AccNo is manually applied. Can be done automatically when constructors are added.
         public int AccNo { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -16,6 +17,7 @@ namespace OOPexam
 
         public abstract bool CalculateInterest();
 
+        // Displaying the class as a string. Used for the lisbox.
         public override string ToString()
         {
             return $"{AccNo} - {LastName}, {FirstName}"; 
@@ -24,14 +26,19 @@ namespace OOPexam
 
     public class CurrentAccount : Account
     {
+        // Interest rate of current account, 3%
         public decimal InterestRate = 0.03m;
 
         public override bool CalculateInterest()
         {
+            // Works by removing a year from the current time. If the InterestDate is less than (Now - 1 year), clearly
+            // then it's over a year ago.
             if (InterestDate < DateTime.Now.AddYears(-1))
             {
+                // Set the InterestDate to now, add the interest to the balance.
                 InterestDate = DateTime.Now;
                 Balance = Balance + (Balance * InterestRate);
+                // return successful
                 return true;
             }
             return false;
@@ -40,10 +47,12 @@ namespace OOPexam
     
     public class SavingsAccount : Account
     {
+        // Interest rate of savings account, 6%
         public decimal InterestRate = 0.06m;
 
         public override bool CalculateInterest()
         {
+            // Same as CurrentAccount
             if (InterestDate < DateTime.Now.AddYears(-1))
             {
                 InterestDate = DateTime.Now;
--- OOPexam/MainWindow.xaml.cs
+++ OOPexam/MainWindow.xaml.cs
@@ -15,11 +15,13 @@ using System.Windows.Shapes;
 
 namespace OOPexam
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /* Ronnie Conlon, s00200671
+     * Github Link : https://github.com/redista/OOPexam
+     */
+
     public partial class MainWindow : Window
     {
+        // Initialize the list of accounts, and the filtered list
         List<Account> Accounts = new List<Account>();
         List<Account> FilteredAccounts = new List<Account>();
 
@@ -30,30 +32,40 @@ namespace OOPexam
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            // On window load 
+            
+            // Create two new accounts, (I made one saving one current)
             Account Acc1 = new CurrentAccount() {AccNo = 1, FirstName = "John", LastName = "Doe", Balance = 10000, InterestDate = new DateTime(2019,05,10)};
             Account Acc2 = new SavingsAccount() {AccNo = 2, FirstName = "Jane", LastName = "Lyon", Balance = 20000, InterestDate = new DateTime(2010,10,20)};
 
+            // Add them to the list
             Accounts.Add(Acc1);
             Accounts.Add(Acc2);
 
+            //Set the source of the listbox to the accounts list
             LbxAccounts.ItemsSource = Accounts;
 
+            // Check both boxes. This will call the selection event twice ( it's only 2 accounts right now though )
             CAcheck.IsChecked = true;
             SAcheck.IsChecked = true;
         }
 
         private void Tcheck_Checked(object sender, RoutedEventArgs e)
         {
-            FilteredAccounts.Clear();
+            // Event for Current/Savings Account checkboxes. Filteres the list depending on which are checked.
 
+            // Clear the list of filtered accounts
+            FilteredAccounts.Clear();
             LbxAccounts.ItemsSource = null;
 
+            // If both are checked, it's just the normal list
             if (CAcheck.IsChecked == true && SAcheck.IsChecked == true)
             {
                 LbxAccounts.ItemsSource = Accounts;
             }
             else
             {
+                // If current is checked
                 if (CAcheck.IsChecked == true)
                 {
                     foreach (Account account in Accounts)
@@ -62,6 +74,7 @@ namespace OOPexam
                             FilteredAccounts.Add(account);
                     }
                 }
+                // If savings is checked
                 else if (SAcheck.IsChecked == true)
                 {
                     foreach (Account account in Accounts)
@@ -71,6 +84,7 @@ namespace OOPexam
                     }
                 }
 
+                // If neither are checked, the list will be empty anyways
                 LbxAccounts.ItemsSource = FilteredAccounts;
             }
 
@@ -78,41 +92,42 @@ namespace OOPexam
 
         private void LbxAccounts_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            // Event for changing the selected item in the listbox
+            // Adds values of selected item to the textbocks
+
+            // Get the selected account
             Account SelectedAccount = LbxAccounts.SelectedItem as Account;
 
-            // If it is null, don't do anything
+            // If it not null, set the textblocks to the values of the account
             if (SelectedAccount != null)
             {
                 FNtb.Text = SelectedAccount.FirstName;
                 LNtb.Text = SelectedAccount.LastName;
                 Balancetb.Text = SelectedAccount.Balance.ToString();
                 InterestDatetb.Text = SelectedAccount.InterestDate.ToString();
+                
+                // If the account is a current account, set the text as a current account, otherwise right now it's a savings account.
                 AccountTypetb.Text = SelectedAccount is CurrentAccount ? "Current" : "Savings";
             }
 
         }
 
-        public void Clear()
-        {
-            FNtb.Text = "";
-            LNtb.Text = "";
-            Balancetb.Text = "";
-            AccountTypetb.Text = "";
-            InterestDatetb.Text = "";
-        }
-
         private void DepositBtn_Click(object sender, RoutedEventArgs e)
         {
+            // Click event for Deposit button. This adds the transaction amount in the tbx to the balance of the selected account
+
             Account SelectedAccount = LbxAccounts.SelectedItem as Account;
 
             decimal TransactionAmount = 0;
 
             if (SelectedAccount != null)
             {
+                // Try to parse the text into a decimal
                 if (decimal.TryParse(Transactiontbx.Text, out TransactionAmount))
                 {
                     SelectedAccount.Balance += TransactionAmount;
 
+                    // Update the showed balance. Empty the transaction field
                     Balancetb.Text = SelectedAccount.Balance.ToString();
                     Transactiontbx.Text = "";
                 }
@@ -121,16 +136,20 @@ namespace OOPexam
 
         private void WithdrawBtn_Click(object sender, RoutedEventArgs e)
         {
+            // Click event for withdraw button. Removes the transaction tbx amount from the balance of the account
+
             Account SelectedAccount = LbxAccounts.SelectedItem as Account;
 
             decimal TransactionAmount = 0;
 
             if (SelectedAccount != null)
             {
+                // Try to parse the tbx value into a decimal
                 if (decimal.TryParse(Transactiontbx.Text, out TransactionAmount))
                 {
                     SelectedAccount.Balance -= TransactionAmount;
 
+                    // Update tb fields
                     Balancetb.Text = SelectedAccount.Balance.ToString();
                     Transactiontbx.Text = "";
                 }
@@ -139,20 +158,24 @@ namespace OOPexam
 
         private void InterestBtn_Click(object sender, RoutedEventArgs e)
         {
-            
+            // Click Event for interest button. This adds the interest to the current balance only if the last time 
+            // the interest was updated was over a year ago.
 
             Account SelectedAccount = LbxAccounts.SelectedItem as Account;
 
             if (SelectedAccount != null)
             {
-                if (!SelectedAccount.CalculateInterest())
+                // This will call the CalculateInterest method. It returns true if successful, false otherwise
+                if (SelectedAccount.CalculateInterest())
                 {
-                    MessageBox.Show("Interest has already been applied within the past year");
+                    // Update the tb fields. 
+                    Balancetb.Text = SelectedAccount.Balance.ToString();
+                    InterestDatetb.Text = SelectedAccount.InterestDate.ToString();
                 }
                 else
                 {
-                    Balancetb.Text = SelectedAccount.Balance.ToString();
-                    InterestDatetb.Text = SelectedAccount.InterestDate.ToString();
+                    // Error box since interest has been applied within the year.
+                    MessageBox.Show("Interest has already been applied within the past year");
                 }
             }
         }
